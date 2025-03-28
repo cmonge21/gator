9655ebcb98acddd9a06ac6da7db4ceba805e8619
cmonge21 <carlypres@gmail.com>
2025-03-28 10:05:44 -0300

    adding login username func

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,7 +8,7 @@ import (
 )
 
 type Config struct {
-	DatabaseURL     string `json:"db_url"`
+	DBURL           string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
 }
 
--- main.go
+++ main.go
@@ -1,11 +1,27 @@
+package main
+
 import (
-	"gator/internal/config/config.go"
 	"fmt"
+	"log"
+
+	"github.com/cmonge21/gator/internal/config"
 )
 
 func main() {
-	cfg := config.Read()
-	cfg.SetUser("Carly")
-	result := Read()
-	fmt.Println(result)
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("Error reading config: %v", err)
+	}
+
+	err = cfg.SetUser("Carly")
+	if err != nil {
+		log.Fatalf("Error setting user: %v", err)
+	}
+
+	updatedCfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("Error reading updated config: %v", err)
+	}
+
+	fmt.Println(updatedCfg.DBURL)
 }
